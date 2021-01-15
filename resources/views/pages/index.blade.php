0542ca25630fd1073e9d5167e7336042ed64ed56
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-01-15 23:13:45 +0100

    create index page

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name'), 'Larablog' }}</title>
+</head>
+<body>
+    <h1>Welcome to Larablog</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum atque aperiam laudantium quas quaerat possimus beatae dolor perspiciatis. Assumenda, tenetur?</p>
+    
+</body>
+</html>
