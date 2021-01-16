2c84b00004e7db561b2e65ace3fcb2c1e0788a2e
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-01-16 18:32:34 +0100

    Create navigation with Bootstrap navigation

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,24 @@
+
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">{{ config('app.name', 'Larablog') }}</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+  
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li>
+            <a class="nav-link" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/services">Services</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
