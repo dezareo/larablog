149005db8deff1b4ac3d6f5444fac714a672a909
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-01-16 15:54:27 +0100

    input blade components into pages

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Welcome to Larablog</h1>
+<h1>{{ $title }}</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea veritatis delectus sit ratione quia animi eveniet velit corrupti omnis possimus!</p>
 @endsection
