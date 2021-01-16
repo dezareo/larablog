575f9d447d9f97c11a7d96167fb51b44669823fb
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-01-16 18:30:57 +0100

    Create final design for index page
    Create Jumbotron and two buttons that leads to login and register page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>{{ $title }}</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea veritatis delectus sit ratione quia animi eveniet velit corrupti omnis possimus!</p>
+<div class="jumbotron text-center">
+    <h1>{{ $title }}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea veritatis delectus sit ratione quia animi eveniet velit corrupti omnis possimus!</p>
+    <p><a href="/login" class="btn btn-primary btn-lg" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+</div>
 @endsection
