c91a0a3f0c0d2760300b5543e63198b8015301a3
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-01-18 23:01:52 +0100

    Create single blog post blade template

---
+++ resources/views/posts/single.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{ $post->title }}</h1>
+    <div class="mt-3 mb-1">
+        {{ $post->body }}
+    </div>
+    <hr>
+    <small>Written on {{ $post->created_at }}</small>
+    <br>
+    <a href="/posts" class="btn btn-outline-secondary btn-sm mt-1">Go Back</a>
+@endsection
