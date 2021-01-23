fdb3193749c4ecda6da825bdb05ac72904ee340f
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-01-23 21:12:47 +0100

    Create writen by in single.blade.php

--- resources/views/posts/single.blade.php
+++ resources/views/posts/single.blade.php
@@ -6,14 +6,14 @@
         {!! $post->body !!}
     </div>
     <hr>
-    <small>Written on {{ $post->created_at }}</small>
+    <small>Written on {{ $post->created_at }} by {{ $post->user->name }}</small>
     <br>
     <div class="row ml-1">
         <a href="/posts" class="btn btn-outline-secondary btn-sm mt-1">Go Back</a> 
         <a href="/posts/{{ $post->id }}/edit" class="btn btn-outline-success btn-sm mt-1 ml-2">Edit</a>
         {!! Form::open(['action' => ['App\Http\Controllers\PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'ml-auto']) !!}
             {{ Form::hidden('_method', 'DELETE') }}
-            {{ Form::submit('Delete', ['class' => 'btn btn-outline-danger btn-sm mt-1']) }}
+            {{ Form::submit('Delete', ['class' => 'btn btn-outline-danger btn-sm mt-1 pull-right']) }}
         {!! Form::close() !!}
     </div>
     
