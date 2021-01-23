68deb18765a20eee34a990dccbdbae64803fb0c4
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-01-23 21:15:14 +0100

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,9 @@ class Post extends Model
     //Timestamps
     public $timestamps = true;
 
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
     
     use HasFactory;
 }
