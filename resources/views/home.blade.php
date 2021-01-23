390572e2cfe4630855c5f5dd8cd15e3e1daad69c
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-01-23 21:14:56 +0100

    Create blog listing into dashboard section with edit and delete buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,29 @@
                 <div class="card-body">
                     <a href="/posts/create" class="btn btn-primary mt-2 mb-3">Create Post</a>
                     <h3>Your Blog Posts</h3>
+                    @if(count($posts) > 0)
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Title</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        @foreach($posts as $post)
+                        <tr>
+                            <td>{{ $post->title }}</td>
+                            <td><a href="/posts/{{ $post->id }}/edit" class="btn btn-outline-success btn-sm pull-right">Edit</a></td>
+                            <td>
+                                {!! Form::open(['action' => ['App\Http\Controllers\PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'ml-auto']) !!}
+                                {{ Form::hidden('_method', 'DELETE') }}
+                                {{ Form::submit('Delete', ['class' => 'btn btn-outline-danger btn-sm pull-right']) }}
+                            {!! Form::close() !!}
+                            </td>
+                        </tr>
+                        @endforeach
+                    </table>
+                    @else
+                        <p>You have no posts</p>
+                    @endif
                 </div>
             </div>
         </div>
